be81f23bc5fd2b1a2d8aa69ea07e3c2e77a129d4
programadorgit <116317487+programadorgit@users.noreply.github.com>
2023-10-18 21:44:13 -0400

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const hamburguer = document.querySelector('.hamburguer');
+const menu= document.querySelector('.menu-navegacion');
+
+
+console.log(menu)
+console.log(hamburguer)
+
+hamburguer.addEventListener('click', ()=>{
+
+    menu.classList.toggle("spread")
+
+})
